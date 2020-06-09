4b8140da8a85d2f6596f353198a3d08d4417884f
João Guilherme <j4g3dev@gmail.com>
2020-06-08 23:28:21 -0300

    Start project

---
+++ mobile/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello World.</Text>
+    </SafeAreaView>
+  );
+}
