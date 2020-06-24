cc433db8dd00f9e089d89c3851ca2921330e708a
João Guilherme <j4g3dev@gmail.com>
2020-06-24 00:00:10 -0300

    Add: routes and functions in backend

--- scripts/backend/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const routes = Router();
-
-routes.get("/",(req,res)=>{
-  res.send("Routes ok")
-})
-
-export default routes;
---
+++ scripts/software/backend/src/routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { spawnSync } from "child_process";
+const running = "Is running command.";
+const routes = Router();
+
+routes.get("/online", (req, res) => {
+  res.send({ status: "Is running." });
+});
+
+routes.get("/shutdown", (req, res) => {
+  res.send({ data: running });
+  spawnSync("shutdown 0");
+});
+
+routes.get("/logout", (req, res) => {
+  res.send({ data: running });
+  spawnSync("gnome-session-quit");
+});
+
+export default routes;
