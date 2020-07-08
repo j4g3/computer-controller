d1ebf9fa441f11ac00a2c5b365359274d1f67f48
João Guilherme <j4g3dev@gmail.com>
2020-07-07 22:11:10 -0300

    Update: App.js add eslint disable

--- scripts/software/src/App.js
+++ scripts/software/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import BaseRequestOfApiForTest from "./services/api";
 import {
   Container,
@@ -7,31 +7,31 @@ import {
   IpStatusOnline,
   IpStatusOffline,
 } from "./style";
-function App() {
+
+const App = () => {
   const [IP, setIP] = useState("");
   const [status, setStatus] = useState(false);
-  useEffect(() => {
-    function ip() {
-      var address,
-        ifaces = require("os").networkInterfaces();
-      for (var dev in ifaces) {
-        ifaces[dev].filter((details) =>
-          details.family === "IPv4" && details.internal === false
-            ? (address = details.address)
-            : undefined
-        );
-      }
-      return address;
+
+  var address,
+    ifaces = require("os").networkInterfaces();
+  for (var dev in ifaces) {
+    // eslint-disable-next-line
+    ifaces[dev].filter((details) =>
+      details.family === "IPv4" && details.internal === false
+        ? (address = details.address)
+        : undefined
+    );
+  }
+  setIP(address);
+
+  async function TestStatusOfApi() {
+    const ResponseRequest = await BaseRequestOfApiForTest.get("/online");
+    if (ResponseRequest.status === 200) {
+      setStatus(true);
     }
-    async function TestStatusOfApi() {
-      const ResponseRequest = await BaseRequestOfApiForTest.get("/online");
-      if (ResponseRequest.status === 200) {
-        setStatus(true);
-      }
-    }
-    TestStatusOfApi();
-    setIP(ip());
-  }, []);
+  }
+  TestStatusOfApi();
+
   return (
     <Container>
       <IpBox>
@@ -44,6 +44,6 @@ function App() {
       </IpBox>
     </Container>
   );
-}
+};
 
 export default App;
